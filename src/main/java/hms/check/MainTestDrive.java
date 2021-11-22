4b6f0000ff4c6ea69e912a1160cb9a449e9d80ed
yejin0528 <74442264+yejin0528@users.noreply.github.com>
2021-11-22 21:02:28 +0900

    Make pay() method

--- src/main/java/hms/check/MainTestDrive.java
+++ src/main/java/hms/check/MainTestDrive.java
@@ -1,24 +1,17 @@
 
 package hms.check;
 
-import hms.room.Reserve;
-import hms.room.Room;
 import java.io.BufferedReader;
-import java.io.File;
-import java.util.ArrayList;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.List;
 
 public class MainTestDrive{
     public static void main(String[] args) throws IOException {
         
         System.out.println("============================================================");
-        System.out.println("                              [체크인]");
+        System.out.println("                                          [체크인]");
         System.out.println("============================================================");
-        System.out.print("체크인 할 객실 번호를 입력해주세요: ");
+        System.out.print(">> 체크인 할 객실 번호를 입력해주세요: ");
         
         //사용자 입력
         BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
