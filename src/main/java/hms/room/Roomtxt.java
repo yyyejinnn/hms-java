0d1a98bed91202abfd5123101e552f33c9342dfe
yejin0528 <74442264+yejin0528@users.noreply.github.com>
2021-11-30 17:50:25 +0900

    Update pay Metod

---
+++ src/main/java/hms/room/Roomtxt.java
@@ -0,0 +1,36 @@
+package hms.room;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LYJ
+ */
+public class Roomtxt {
+    private int roomIdx;
+    private int maxPeopleNum;
+    private int fee;
+    private String roomClean;
+
+    public int getRoomIdx() {
+        return roomIdx;
+    }
+
+    public int getMaxPeopleNum() {
+        return maxPeopleNum;
+    }
+
+    public int getFee() {
+        return fee;
+    }
+
+    public String getRoomClean() {
+        return roomClean;
+    }
+    
+}
+
